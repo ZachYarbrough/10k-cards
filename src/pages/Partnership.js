3cc13ad6752087f6c17611e027c8ca62a4129c9a
Zach Yarbrough <zachyarbro@gmail.com>
2022-04-06 08:40:37 -0500

    Fixed React errors

--- src/pages/Partnership.js
+++ src/pages/Partnership.js
@@ -1,13 +1,15 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 
 const Partnership = () => {
+    const navigate = useNavigate();
     return (
         <Box sx={{ width: '80%', textAlign: 'center', mx: 'auto', mt: 5 }}>
             <Typography variant="h5" sx={{ mb: 1, color: 'grey.600' }}>10k Zipcode Partnerships coming soon!</Typography>
-            <Button variant="contained" color="secondary">Back to Home</Button>
+            <Button variant="contained" onClick={() => navigate('/')} color="secondary">Back to Home</Button>
         </Box>
     )
 }
