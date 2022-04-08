776721ffb0f573788daaf7bdb0ec6a4d49a70dee
Zach Yarbrough <zachyarbro@gmail.com>
2022-04-08 11:41:56 -0500

    Update Feature Text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = ({ cart, setCart, sum, setSum }) => {
     const navigate = useNavigate();
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center', width: '100%', minHeight: { xs: '70vh'} }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center', width: '100%', minHeight: { xs: '70vh' } }}>
             <Typography variant="h1" sx={{ fontSize: { xs: '4vh', md: '5vh' }, fontWeight: 400, mt: 6 }}>The Modern Day Business Card</Typography>
             <Typography variant="h2" sx={{ fontSize: '2vh', fontWeight: '400', mt: 2, mb: 5 }}>The Easiest way to Spread your Name Across the World</Typography>
             <Grid container sx={{ ml: { xs: 0, md: 5 }, mb: 5, width: '70%' }}>
@@ -39,12 +39,11 @@ const Home = ({ cart, setCart, sum, setSum }) => {
             <ProductTable cart={cart} setCart={setCart} />
             <FeaturedItems cart={cart} setCart={setCart} sum={sum} setSum={setSum} />
             <Grid container sx={{ ml: { xs: 0, md: 5 }, mb: 5, width: '70%' }}>
-                <Grid data-aos='fade-right' item sm={12} md={6} sx={{ display: { xs: 'none', md: 'flex'}, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                    <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>The Central Point of Your Networking</Typography>
-                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>From WhatsApp to Instagram to LinkedIn, provide people with the ability to connect with you like never before from one central hub.</Typography>
-                    <Button sx={{ mb: { xs: 0, md: 3 } }} variant="contained" color="secondary" onClick={() => navigate('/edit')}>Create Sample Card</Button>
+                <Grid data-aos='fade-right' item sm={12} md={6} sx={{ display: { xs: 'none', md: 'flex' }, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>Let Your Clients Engage with You</Typography>
+                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>You have an infinite amount of ways to allow users interact with you, limited only by your imagination. Let customers pay you with Zelle, Venmo, or Cash App. Book an appointment with your booking system. Provide directions to your address, add links to your products, and much more!</Typography>
                 </Grid>
-                <Grid data-aos='fade-left' item md={6} sx={{ mb: { xs: 5, md: 0 }, display: { xs: 'none', md: 'block'} }}>
+                <Grid data-aos='fade-left' item md={6} sx={{ mb: { xs: 5, md: 0 }, display: { xs: 'none', md: 'block' } }}>
                     <Box
                         component="img"
                         sx={{
@@ -55,7 +54,7 @@ const Home = ({ cart, setCart, sum, setSum }) => {
                         src={ExampleProduct}
                     />
                 </Grid>
-                <Grid data-aos='fade-right' item xs={12} sx={{ mb: { xs: 5, md: 0 },  display: { xs: 'block', md: 'none'} }}>
+                <Grid data-aos='fade-right' item xs={12} sx={{ mb: { xs: 5, md: 0 }, display: { xs: 'block', md: 'none' } }}>
                     <Box
                         component="img"
                         sx={{
@@ -66,14 +65,12 @@ const Home = ({ cart, setCart, sum, setSum }) => {
                         src={ExampleProduct}
                     />
                 </Grid>
-                <Grid data-aos='fade-left' item sm={12} md={6} sx={{ display: { xs: 'flex', md: 'none'}, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                    <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>The Central Point of Your Networking</Typography>
-                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>From WhatsApp to Instagram to LinkedIn, provide people with the ability to connect with you like never before from one central hub.</Typography>
-                    <Button sx={{ mb: { xs: 0, md: 3 } }} variant="contained" color="secondary" onClick={() => navigate('/edit')}>Create Sample Card</Button>
-                </Grid>
+                <Grid data-aos='fade-left' item sm={12} md={6} sx={{ display: { xs: 'flex', md: 'none' }, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>Let Your Clients Engage with You</Typography>
+                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>You have an infinite amount of ways to allow users interact with you, limited only by your imagination. Let customers pay you with Zelle, Venmo, or Cash App. Book an appointment with your booking system. Provide directions to your address, add links to your products, and much more!</Typography>                </Grid>
             </Grid>
-            <Box sx={{ width: '100%', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>
-                <Grid container sx={{mb: 5, width: '70%', mx: 'auto' }}>
+            <Box sx={{ width: '100%', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+                <Grid container sx={{ mb: 5, width: '70%', mx: 'auto' }}>
                     <Grid data-aos='fade-right' item xs={12} md={6} sx={{ mb: { xs: 5, md: 0 } }}>
                         <Box
                             component="img"
@@ -86,19 +83,18 @@ const Home = ({ cart, setCart, sum, setSum }) => {
                         />
                     </Grid>
                     <Grid data-aos='fade-left' item sm={12} md={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                        <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>The Central Point of Your Networking</Typography>
-                        <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>From WhatsApp to Instagram to LinkedIn, provide people with the ability to connect with you like never before from one central hub.</Typography>
-                        <Button sx={{ mb: { xs: 0, md: 3 } }} variant="contained" color="secondary" onClick={() => navigate('/edit')}>Create Sample Card</Button>
+                        <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>Make 20% Profit on Each Referral</Typography>
+                        <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>Any user that clicks on the referral button of your digital card and completes a purchase will earn you 20% of each order. Sign up for your premium or gold card today!</Typography>
                     </Grid>
                 </Grid>
             </Box>
             <Grid container sx={{ ml: { xs: 0, md: 5 }, mb: 5, width: '70%' }}>
-                <Grid data-aos='fade-right' item sm={12} md={6} sx={{ display: { xs: 'none', md: 'flex'}, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                    <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>The Central Point of Your Networking</Typography>
-                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>From WhatsApp to Instagram to LinkedIn, provide people with the ability to connect with you like never before from one central hub.</Typography>
-                    <Button sx={{ mb: { xs: 0, md: 3 } }} variant="contained" color="secondary" onClick={() => navigate('/edit')}>Create Sample Card</Button>
+                <Grid data-aos='fade-right' item sm={12} md={6} sx={{ display: { xs: 'none', md: 'flex' }, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>The Easiest Way to Spread Your Name Across the World </Typography>
+                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>Anyone with a smartphone camera is able to scan your customized Q-Code™ to get immediate access to your card.
+                        Sharing options allow you and your users to share your card to any platform with a click of a button.</Typography>
                 </Grid>
-                <Grid data-aos='fade-left' item md={6} sx={{ mb: { xs: 5, md: 0 }, display: { xs: 'none', md: 'block'} }}>
+                <Grid data-aos='fade-left' item md={6} sx={{ mb: { xs: 5, md: 0 }, display: { xs: 'none', md: 'block' } }}>
                     <Box
                         component="img"
                         sx={{
@@ -109,7 +105,7 @@ const Home = ({ cart, setCart, sum, setSum }) => {
                         src={ExampleProduct}
                     />
                 </Grid>
-                <Grid data-aos='fade-right' item xs={12} sx={{ mb: { xs: 5, md: 0 },  display: { xs: 'block', md: 'none'} }}>
+                <Grid data-aos='fade-right' item xs={12} sx={{ mb: { xs: 5, md: 0 }, display: { xs: 'block', md: 'none' } }}>
                     <Box
                         component="img"
                         sx={{
@@ -120,10 +116,10 @@ const Home = ({ cart, setCart, sum, setSum }) => {
                         src={ExampleProduct}
                     />
                 </Grid>
-                <Grid data-aos='fade-left' item sm={12} md={6} sx={{ display: { xs: 'flex', md: 'none'}, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                    <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>The Central Point of Your Networking</Typography>
-                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>From WhatsApp to Instagram to LinkedIn, provide people with the ability to connect with you like never before from one central hub.</Typography>
-                    <Button sx={{ mb: { xs: 0, md: 3 } }} variant="contained" color="secondary" onClick={() => navigate('/edit')}>Create Sample Card</Button>
+                <Grid data-aos='fade-left' item sm={12} md={6} sx={{ display: { xs: 'flex', md: 'none' }, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography variant='h3' sx={{ fontSize: { xs: '2.5vh', md: '3vh' }, fontWeight: 500, mb: 1 }}>The Easiest Way to Spread Your Name Across the World </Typography>
+                    <Typography sx={{ fontSize: { xs: '1.8vh', md: '2vh' }, mb: 2 }}>Anyone with a smartphone camera is able to scan your customized Q-Code™ to get immediate access to your card.
+                        Sharing options allow you and your users to share your card to any platform with a click of a button.</Typography>
                 </Grid>
             </Grid>
         </Box>
